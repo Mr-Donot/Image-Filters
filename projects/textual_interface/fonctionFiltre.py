f59bb79e02c1c22ef9a96ebfc300c352c56ef084
Florian <florian.daunay@etu.univ-nantes.fr>
2023-06-09 18:45:35 +0200

    Add custom filter function

--- projects/textual_interface/fonctionFiltre.py
+++ projects/textual_interface/fonctionFiltre.py
@@ -15,14 +15,43 @@ def filtreNegatif(monImage):
     img.show()
     saveImg(img)
 
+def filterWithMask(monImage, mask):
+    mask = normalizeMask(mask)
+    print(mask)
+    (largeur,hauteur)=monImage.size
+    img = Image.new('RGB', (largeur,hauteur), color = (0,0,0))
+    for i in range (0,largeur):
+        for j in range (0,hauteur):
+            p=monImage.getpixel((i,j))
+            img.putpixel((i,j),(int(p[0]*mask[0]),int(p[1]*mask[1]),int(p[2]*mask[2])))
+    img.show()
+    saveImg(img)
+
+def normalizeMask(mask):
+    temp = max(mask)
+    return [mask[0]/temp, mask[1]/temp, mask[2]/temp]
+
+
 def filtreVert(monImage):
+  
+    (largeur,hauteur)=monImage.size
+    img = Image.new('RGB', (largeur,hauteur), color = (0, 1, 0))
+    for i in range (0,largeur):
+        for j in range (0,hauteur):
+            pTemp = img.getpixel((i,j))
+            p=monImage.getpixel((i,j))
+            img.putpixel((i,j),(p[0]*pTemp[0],p[1]*pTemp[1],p[2]*pTemp[2]))
+    img.show()
+    saveImg(img)
+
+def filtreVertOld(monImage):
   
     (largeur,hauteur)=monImage.size
     img = Image.new('RGB', (largeur,hauteur), color = (0, 0, 0))
     for i in range (0,largeur):
         for j in range (0,hauteur):
             p=monImage.getpixel((i,j))
-            img.putpixel((i,j),(0,p[1],0))
+            img.putpixel((i,j),(0,p[1], 0))
     img.show()
     saveImg(img)
 
