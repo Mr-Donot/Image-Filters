563438e8e43ac391ae3792cb65d3197e3b431289
Florian <florian.daunay37@gmail.com>
2023-06-17 12:45:33 +0200

    Add in-depth traversal

--- projects/MenuManager.py
+++ projects/MenuManager.py
@@ -2,8 +2,8 @@ class MenuManager:
     def __init__(self, tree):
         self.tree = tree
 
-    def displayMenu(self):
-        dict = self.tree
+    def displayMenu(self, tree=None):
+        dict = self.tree if tree == None else tree
         for index in dict:
             branch = dict[index]
             print(f"{branch['name']} ({list(dict.keys())[list(dict.values()).index(branch)]})")
@@ -11,9 +11,12 @@ class MenuManager:
         if listener in dict.keys():
             if dict[listener]["type"] == "function" : 
                 dict[listener]["function"]()
+                self.displayMenu()
+            elif dict[listener]["type"] == "folder" :
+                self.displayMenu(dict[listener]["children"])
+
         else :
-            quit = input("Do you want to quit ? (Y/n)")
+            quit = input("Do you want to quit ? (Y/n) : ")
             quits  = ["y", "yes", "ouais", "q", "quit", "quitter", "exit", "bye", "oui", "si"]
-            if quit.lower() in quits:
-               return
-        self.displayMenu()
+            if quit.lower() not in quits:
+                self.displayMenu()
